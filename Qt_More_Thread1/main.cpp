d3d51edb392e993f866ea24571319fa45cc191c6
tangyongchao <tangyongchao68@163.com>
2019-04-18 10:59:01 +0800

    Creat a thread in main.cpp

--- Qt_More_Thread1/main.cpp
+++ Qt_More_Thread1/main.cpp
@@ -1,11 +1,30 @@
 #include <QtCore/QCoreApplication>
 
+#include <mythread.h>
+#include <QDebug>
+#include <QMutex>
+#include <QThread>
+
+
 #include "widget.h"
 #include <QApplication>
 
 int main(int argc, char *argv[])
 {
 	QApplication a(argc, argv);
+	/*线程初始化*/
+	/*
+	MyThread *thread1;
+	thread1 = new MyThread;
+	thread1->start();
+	qDebug() << "Main Thread id：" << QThread::currentThreadId();
+
+	while (1)
+	{
+		qDebug() << "111==" << QThread::currentThreadId();
+		_sleep(1);
+	}
+*/
 	Widget w;
 	w.resize(960, 640);
 	w.show();
